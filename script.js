ed3df0878625f4d3e6255f052bafe00b8b8767a0
Scott Dolphin <nathanjj1225@gmail.com>
2023-09-27 22:53:16 -0700

    please dont break

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var queue = "";
-var finished_calculating = false;
+var finished_calculating = true;
 var operator = "";
-var nu1 = 0;
+var num1 = 0;
+var num2 = 0
+
 
 function Clear() {
     document.getElementById("result").innerText = "‎";
@@ -9,7 +11,12 @@ function Clear() {
     queue = "";
     operator = "";
     console.log("queue: " + queue);
-}    /* 
+    console.log("num1: " + num1);
+    console.log("num2: " + num2);
+    
+
+}
+/* 
 ===================================================================
 
 FIX ME: Calculator only calculates the last 3 chars
@@ -38,6 +45,13 @@ directly
 */
 
 function getNum(value) {
+
+    if (finished_calculating) {
+        queue = "";
+        num1 = "";
+        num2 = "";
+    }
+    finished_calculating = false;
     let x = value;
     console.log("received " + x);
     queue += (x);
@@ -48,35 +62,82 @@ function getNum(value) {
 }
 
 function getOp(value) {
+
     num1 = queue;
-    queue = "";
+
     operator = value;
-    console.log("current operator: " + operator)
+    console.log("current operator: " + operator);
+    console.log("current num1: " + num1);
 
     switch (operator) {
         case "+":
             document.getElementById("result").innerText = num1 + "+";
-            console.log(1);
+            // console.log(1);                  for debugging
             break;
         case "-":
             document.getElementById("result").innerText = num1 + "-";
-            console.log(2);
+            // console.log(2);                  for debugging
             break;
         case "x":
             document.getElementById("result").innerText = num1 + "x";
-            console.log(3);
+            // console.log(3);                  for debugging
             break;
         case "÷":
             document.getElementById("result").innerText = num1 + "÷";
-            console.log(4);
+            // console.log(4);                  for debugging
             break;
 
     }
+    queue = "";
+    console.log("queue cleared");
+    finished_calculating = false;
 
 
 }
 
+function calculate() {
+    console.log("calling calculate()");
+    num2 = queue;
 
+    console.log("current num1: " + num1);
+    console.log("current num2: " + num2);
+
+    if (num2 == "") {
+        num2 = num1;
+    }
+
+    let ans = 0;
+    switch (operator) {
+        case "+":
+            ans = parseInt(num1) + parseInt(num2);
+            console.log(1);
+            break;
+
+        case "-":
+            ans = parseInt(num1) - parseInt(num2);
+            console.log(2);
+            break;
+
+        case "x":
+            ans = parseInt(num1) * parseInt(num2);
+            console.log(3);
+            break;
+
+        case "÷":
+            if (num2 == "0") {
+                document.getElementById("result").innerText = "divide by zero";
+                console.log("divide by zero")
+            }
+
+            ans = parseInt(num1) / parseInt(num2);
+            console.log(4);
+            break;
+    }
+    console.log("answer: " + ans);
+    document.getElementById("result").innerText = ans;
+    queue = ans;
+    finished_calculating = true;
+}
 
 
 
